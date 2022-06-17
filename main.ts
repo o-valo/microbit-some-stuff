8d805cd52408c2507a1ce9fe894c02027f637b28
o-valo <94016567+o-valo@users.noreply.github.com>
2022-06-17 18:50:13 +0200

    thermostat-v1-fu-1

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let t1 = 0
 let tx = 0
 basic.forever(function () {
+    radio.setGroup(1)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -13,8 +14,10 @@ basic.forever(function () {
     if (t1 <= tx) {
         basic.showIcon(IconNames.StickFigure)
         basic.showNumber(t1)
+        radio.sendString("" + (t1))
     } else {
         basic.showIcon(IconNames.Heart)
         basic.showNumber(t1)
+        radio.sendString("" + (t1))
     }
 })
